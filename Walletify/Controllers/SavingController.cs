e57a0129d4f2c55cad4f25396ff26f02bcaabd70
ibraheem Zeer <137792310+ibraheem-zeer@users.noreply.github.com>
2024-09-26 20:23:38 +0300

    icon&controller

---
+++ Walletify/Controllers/SavingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Walletify.Controllers
+{
+    public class SavingController : Controller
+    {
+        public IActionResult SetValue()
+        {
+            return View();
+        }
+        public IActionResult GetValue()
+        {
+            return View();
+        }
+
+        public IActionResult ChangeValue()
+        {
+            return View();
+        }
+    }
+}
