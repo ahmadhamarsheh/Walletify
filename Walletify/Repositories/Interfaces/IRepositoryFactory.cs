d982a23d73a034e76eb9d327e26220c6b774e839
Hajar Alhajeh <hajar.ihab@gmail.com>
2024-10-01 22:34:33 +0300

    Update Factory

--- Walletify/Repositories/Interfaces/IRepositoryFactory.cs
+++ Walletify/Repositories/Interfaces/IRepositoryFactory.cs
@@ -1,10 +1,13 @@
-﻿namespace Walletify.Repositories.Interfaces
+﻿using Walletify.Repositories.Implementation;
+
+namespace Walletify.Repositories.Interfaces
 {
     public interface IRepositoryFactory
     {
         IUserRepository User { get; }
         ICategoryRepository Category { get; }
         ITransactionRepository Transaction { get; }
+        IAccountRepositry Account { get; }
         void Save();
     }
 }
