6cbbae930b7417ae628c84ea0064e7ebd3e31dcb
Hajar Alhajeh <hajar.ihab@gmail.com>
2024-10-02 22:54:14 +0300

    add Category and Seeding initial data for Category with CategoryType

--- Walletify/Models/Entities/Category.cs
+++ Walletify/Models/Entities/Category.cs
@@ -2,11 +2,23 @@
 
 namespace Walletify.Models.Entities
 {
+    public enum CategoryType
+    {
+        Spending,
+        Income
+    }
     public class Category
     {
+
         public int Id { get; set; }
         [Required]
         [MaxLength(20, ErrorMessage = "Category Name must be less than 20 chars")]
         public string Name { get; set; }
+
+        [Required]
+        public CategoryType CategoryType { get; set; }
+
+
+
     }
 }
